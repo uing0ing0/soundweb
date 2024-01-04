b5e07bfe4c7ba663e31bbe34dabf9c2a43438c03
Shim su yeon <suyeonshim@gm.gist.ac.kr>
2024-01-04 21:34:02 +0900

    google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import "./App.css";
 
+import ReactGA from "react-ga";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import Login from "./pages/login/Login";
 //import { useState } from "react";
 //import { useTranslation } from "react-i18next";
 //import styled from "styled-components";
 import Main from "./pages/main/main";
-import Login from "./pages/login/Login";
 // const ReadTheDocs = styled.p`
 //   color: #888;
 // `;
+const gaTrackingId = process.env.REACT_APP_GA_TRACKING_ID || "";
+ReactGA.initialize(gaTrackingId, { debug: true });
+ReactGA.pageview(window.location.pathname);
 
 function App() {
   //const [count, setCount] = useState(0);
@@ -22,10 +26,9 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
         </Routes>
-        
       </BrowserRouter>
     </>
-    );
+  );
 }
 
 export default App;
