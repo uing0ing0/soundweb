661b0304c84e56164cb921a7c2df82e61c84d45a
Shim su yeon <suyeonshim@gm.gist.ac.kr>
2024-01-04 13:27:20 +0900

    initial outline - navbar main login

---
+++ src/pages/main/main.tsx
@@ -0,0 +1,14 @@
+import Navbar from "src/atoms/navbar/Navbar";
+
+//import Layout from "src/layout/layout";
+
+const Main = () => {
+  return (
+    <div style={{width:"100%", height:"100%"}}>
+        <Navbar />
+        <h1>Main</h1>
+    </div>
+  );
+};
+
+export default Main;
