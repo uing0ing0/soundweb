0887a15427a673af61e1d545deb2c09c8df4e914
Shim su yeon <suyeonshim@gm.gist.ac.kr>
2024-01-05 21:16:26 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import "./App.css";
 
-import ReactGA from "react-ga";
+//import ReactGA from "react-ga";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Login from "./pages/login/Login";
-//import { useState } from "react";
-//import { useTranslation } from "react-i18next";
-//import styled from "styled-components";
 import Main from "./pages/main/main";
-// const ReadTheDocs = styled.p`
-//   color: #888;
-// `;
-const gaTrackingId = process.env.REACT_APP_GA_TRACKING_ID || "";
-ReactGA.initialize(gaTrackingId, { debug: true });
-ReactGA.pageview(window.location.pathname);
+
+//const gaTrackingId = process.env.REACT_APP_GA_TRACKING_ID || "";
+//ReactGA.initialize(gaTrackingId, { debug: true });
+//ReactGA.pageview(window.location.pathname);
 
 function App() {
-  //const [count, setCount] = useState(0);
-  //const { t } = useTranslation("main");
-
   return (
     <>
       <BrowserRouter>
