1160b79aac5a26d349c0a7b51d97e1bfa8ab06e6
uing0ing0 <sksmsdmsdud91@gist.ac.kr>
2024-01-06 14:12:53 +0900

    전체화면

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,10 +1,49 @@
 import Navbar from "src/atoms/navbar/Navbar";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
 
 const Login = () => {
+  const navigate = useNavigate();
+
   return (
-    <div style={{ width: "100%", height: "100%" }}>
+    <div className="container">
       <Navbar />
-      <h1>Loogin</h1>
+      <div className="form-container">
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter your email"
+        />
+      </div>
+      <div className="form-container">
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Enter your password"
+        />
+      </div>
+
+      <button
+        type="submit"
+        onClick={() => {
+          navigate("/login");
+        }}
+        className="form-container"
+      >
+        Login
+      </button>
+
+      <button
+        type="button"
+        onClick={() => {
+          navigate("/login");
+        }}
+        className="form-container"
+      >
+        Create Account
+      </button>
     </div>
   );
 };
