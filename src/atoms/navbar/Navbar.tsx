bdac6b64e6fc991cc56680e8b4629e9f15525cef
uing0ing0 <sksmsdmsdud91@gist.ac.kr>
2024-01-09 16:55:24 +0900

    대략적 구성

--- src/atoms/navbar/Navbar.tsx
+++ src/atoms/navbar/Navbar.tsx
@@ -22,10 +22,18 @@ const Navbar = () => {
           alignItems: "center",
           fontSize: "1.7rem",
           background: "lightblue",
+          position: "fixed",
+          top: 0,
         }}
       >
         <div style={{ marginRight: "auto" }} onClick={handleLogo}>
           Logo
+        </div>
+        <div style={{ marginRight: "auto" }} onClick={handleLogo}>
+          레전드 해결책
+        </div>
+        <div style={{ marginRight: "auto" }} onClick={handleLogo}>
+          레전드 해결책 만들기
         </div>
         <div style={{ marginLeft: "auto" }} onClick={handleLogin}>
           login
